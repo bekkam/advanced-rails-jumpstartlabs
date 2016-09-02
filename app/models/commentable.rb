06b894ed5ae1119d91a4af5f800ef45c1043825f
Bekka Murphy <bekkam@users.noreply.github.com>
2016-09-02 12:47:53 -0700

    Add instance method to check for comments; update article view accordingly

--- app/models/commentable.rb
+++ app/models/commentable.rb
@@ -3,6 +3,12 @@
 module Commentable
   extend ActiveSupport::Concern
 
+# Define an instance method in the Commentable module named has_comments? which 
+# returns true or false based on the existence of comments. 
+  def has_comments?
+    comments.count > 0 ? true : false
+  end
+
   # when we extend ActiveSupport::Concern, we can call 'included' instead of self.included(including_class)
   included do
     #  extend ClassMethods
